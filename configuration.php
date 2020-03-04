edd2e1774bdacefebef2f9393f59a5e1ace48e5e
Thomas Beck <thomas.beck.1997@gmail.com>
2020-03-04 19:17:26 +0100

    bootstrap, fontawesome added

---
+++ configuration.php
@@ -0,0 +1,9 @@
+<?php
+    class appConfig{
+        /* Database Settings */
+        public $host = 'localhost';
+        public $user = 'cs-34_prj_usr';
+        public $password = '>^+{E>:T4F9-~QmT3!T';
+        public $db = 'cs-343657_project_dhbw';
+    }
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+    require("components/functions.php");
+    generate_header("Startseite", "Herzlich Willkomen zur Online-Vorlesungsplatform der DHBW Ravensburg", null);
+?>
+
+<?php
+    generate_footer();
+?>
